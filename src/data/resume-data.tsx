2d0cc2426a973675f41044e348686984945e54eb
Johann Waldherr <johannwaldherr@mac-2.local>
2024-01-23 16:13:25 +0100

    added logos

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,21 +1,10 @@
 import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
+  riedelLogo,
+  coactiveLogo,
+  infineonLogo,
+  sueLogo,
+  skaleLogo,
+  sLogo,
 } from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
@@ -27,7 +16,7 @@ export const RESUME_DATA = {
   about:
     "Business developer with applied expertise in Computer Science, Artificial Intelligence (AI), and Robotics.",
   summary:
-    "In my role as a Development Engineer, I specialized in designing custom ASICs and played a pivotal role in developing the first version of the optical fiber network at Riedel. Transitioning into Business Development, I have successfully co-founded several startups, securing research funding and venture capital. My expertise extends to building and leading diverse teams, fostering innovation and driving business growth.",
+    "In my role as a Development Engineer, I specialized in designing custom ASICs and developed the first version of an optical fiber network. Transitioning into Business Development, I co-founded several startups, securing research funding and venture capital. My expertise extends to building and leading diverse teams, fostering innovation and driving business growth.",
   avatarUrl: "https://media.licdn.com/dms/image/C5603AQEuKW28-ihawA/profile-displayphoto-shrink_800_800/0/1531836294847?e=1711584000&v=beta&t=QP45JPmvSL8eESn8f8bWxNvOSc5xxHv0o9x2Ns05iNA",
   personalWebsiteUrl: "https://skale.io",
   contact: {
@@ -60,7 +49,7 @@ export const RESUME_DATA = {
     },
     {
       school: "Danube University Krems",
-      degree: "MBA in Entrepreneurship & Innovation",
+      degree: "MBA course in Entrepreneurship & Innovation",
       start: "2004",
       end: "2006",
     }
@@ -71,29 +60,29 @@ export const RESUME_DATA = {
       link: "https://coactive.com",
       badges: ["US", "Silicon Valley"],
       title: "ASIC Designer",
-      logo: ParabolLogo,
+      logo: coactiveLogo,
       start: "1999",
       end: "2000",
       description:
-        "ASIC design for powerline communication",
+        "ASIC design for powerline communication. Integration of a USB IP core into a custom chip design.",
     },
     {
       company: "Infineon Technologies",
       link: "https://infineon.com",
       badges: ["Onsite"],
       title: "ASIC Engineer → DSP Engineer",
-      logo: ClevertechLogo,
+      logo: infineonLogo,
       start: "2001",
       end: "2003",
       description:
-        "DSL modem ASIC design, DSP algorithm development",
+        "Product ownership of a complex DSL modem ASIC design. DSP algorithm development into IP phone equipment.",
     },
     {
       company: "Riedel Mediornet",
       link: "https://riedel.net",
       badges: ["Onsite"],
       title: "System Engineer → Lead System Architect",
-      logo: JojoMobileLogo,
+      logo: riedelLogo,
       start: "2004",
       end: "2007",
       description:
@@ -102,9 +91,9 @@ export const RESUME_DATA = {
     {
       company: "StreamUnlimited Engineering (SUE)",
       link: "https://www.streamunlimited.com",
-      badges: ["Onsite", "Travel"],
+      badges: ["Onsite", "International"],
       title: "International Sales Manager",
-      logo: NSNLogo,
+      logo: sueLogo,
       start: "2007",
       end: "2008",
       description: "Expanded the global sales network for SUE's software and hardware products, achieving a 30% growth in the sales pipeline. Notably, in 2017, Google acquired a stake in SUE.",
@@ -114,7 +103,7 @@ export const RESUME_DATA = {
       link: "https://www.skale.io",
       badges: ["Onsite", "Remote"],
       title: "Owner",
-      logo: NSNLogo,
+      logo: skaleLogo,
       start: "2008",
       end: "ongoing",
       description: "Consulting and coaching for startups and SMEs. Acted as cofounder, business partner, CTO for several startups. Raised research funds and venture capital. Built and led teams.",
@@ -142,7 +131,7 @@ export const RESUME_DATA = {
       techStack: ["Mobile Game", "Education", "GreenTech"],
       description:
         "Conception and financing of a mobile game that teaches kids about renewable energy.",
-      logo: Minimal,
+      logo: sLogo,
       link: {
         label: "greenpilot.at",
         href: "https://www.greenpilot.at/",
@@ -153,7 +142,7 @@ export const RESUME_DATA = {
       techStack: ["AI", "MedTech", "Research Grant"],
       description:
         "Aquisition of seed grant of 500k€ from the Austrian Research Promotion Agency (FFG) for a university spinoff project that uses AI to analyze the retina.",
-      logo: BarepapersLogo,
+      logo: sLogo,
       link: {
         label: "Retinsight",
         href: "https://retinsight.com/",
@@ -163,7 +152,7 @@ export const RESUME_DATA = {
       title: "Pino Robotics",
       techStack: ["AI Vision", "Robotics", "Funding"],
       description: "Funding for an innovative AI vision technology to make industrial robotics usable for SMEs.",
-      logo: YearProgressLogo,
+      logo: sLogo,
       link: {
         label: "cncmonster.eu",
         href: "https://cncmonster.eu/",
@@ -177,7 +166,7 @@ export const RESUME_DATA = {
       ],
       description:
         "Consulting for a startup that builds a marketplace for biodiversity.",
-      logo: ParabolLogo,
+      logo: sLogo,
       link: {
         label: "bionana.at",
         href: "https:/bionana.at/",
@@ -192,7 +181,7 @@ export const RESUME_DATA = {
       ],
       description:
         "Coaching for startups and SMEs in the Blue Economy. BlueInvest is an initiative of the European Commission.",
-      logo: ParabolLogo,
+      logo: sLogo,
       link: {
         label: "blueinvest",
         href: "https://blueinvest-community.converve.io/",
@@ -206,7 +195,7 @@ export const RESUME_DATA = {
       ],
       description:
         "Consulting for a startup that builds europes leading marina rental platform.",
-      logo: ParabolLogo,
+      logo: sLogo,
       link: {
         label: "seasy.com",
         href: "https://seasy.com/",
@@ -221,7 +210,7 @@ export const RESUME_DATA = {
       ],
       description:
         "Led the funding process for a startup that builds a drone based 3D mapping solution for the construction industry.",
-      logo: ParabolLogo,
+      logo: sLogo,
       link: {
         label: "skyability.com",
         href: "https://skyability.com/",
@@ -238,7 +227,7 @@ export const RESUME_DATA = {
         "Venture Capital"
       ],
       description: "A machine learning platform for music recommendation. I cofounded the company, raised research funds and venture capital and built and led the business team.",
-      logo: ConsultlyLogo,
+      logo: sLogo,
       link: {
         label: "sonarflow.com",
         href: "https://sonarflow.com/",
@@ -252,7 +241,7 @@ export const RESUME_DATA = {
         "Business Development"],
       description:
         "Cofounded a company that builds and sells industrial drones. Won client contracts in Switzerland, Norway, UAE, Thailand and China.",
-      logo: MonitoLogo,
+      logo: sLogo,
       link: {
         label: "stromkind.com",
         href: "https://stromkind.com/",
@@ -263,7 +252,7 @@ export const RESUME_DATA = {
       techStack: ["e-mobility", "maritime", "Production Rampup", "Germany"],
       description:
         "Initiated and cofounded a company that builds novel electric jets for maritime applications. Built techteam. Led development from first prototype to production.",
-      logo: JarockiMeLogo,
+      logo: sLogo,
       link: {
         label: "sylents.de",
         href: "https://sylents.de/",
